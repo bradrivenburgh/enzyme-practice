d8eabd46423777ac07efbb6e2926d8ed6af86b3a
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-04-13 17:03:10 -0400

    added Login component and tests

---
+++ src/components/tests/Login.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow, mount, render, simulate } from 'enzyme';
+
+import Login from '../Login';
+
+describe('Login Test Suite', () => {
+  it('should render the form', () => {
+    const wrapper = shallow(<Login />);
+    // renders form element
+    expect(wrapper.find('.login').exists()).toBe(true);
+    // renders an email input and a password input
+    expect(wrapper.find('#email')).toHaveLength(1);
+    expect(wrapper.find('#password')).toHaveLength(1);
+  });
+});
+
+describe('Email Test Suite', () => {
+  it('should change the state of the Login component', () => {
+    const wrapper = shallow(<Login />);
+    // Find 'email' input
+    const email = wrapper.find('#email');
+    // Simulate blur event and pass mock data
+    email.simulate('blur', {
+      target: { name: 'email', value: 'brad.rivenburgh@gmail.com' },
+    });
+    // Assert that state of the email property will update correctly
+    expect(wrapper.state('email')).toEqual('brad.rivenburgh@gmail.com');
+  });
+});
+
+describe('Password Test Suite', () => {
+  const wrapper = mount(<Login />);
+  const password = wrapper.find('#password');
+  password.simulate('blur', {
+    target: { name: 'password', value: 'my_log_is_rocket' },
+  });
+  expect(wrapper.state('password')).toEqual('my_log_is_rocket');
+});
